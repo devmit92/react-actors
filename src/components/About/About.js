876d3ae2ea7a82ea0770067cc22c4ce84a26df16
Devon Mitchell <devonamitchell10@gmail.com>
2019-05-08 09:02:28 -0500

    still working

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <h2>About</h2>
+            <p>
+                We are actors working with actors and we react to things.
+            </p>
+        );
+    }
+}
+
+export default Header;
